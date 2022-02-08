b289e5d66dc85b0bf26e36081206cce2a2e17267
stephenhky <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>
2022-02-07 21:16:29 -0500

    print score

--- sec2/frozenlake_Qlearning.py
+++ sec2/frozenlake_Qlearning.py
@@ -26,7 +26,7 @@ for i in tqdm(range(nbtrials)):
         action = agent.choose_action()
         observation, reward, done, info = agent.next_step(action)
         score += reward
-
+    print(score)
     scores.append(score)
 
     if i % 1000 == 0:
